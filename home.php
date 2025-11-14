e2b922c97c7549a84496551dad0fa68def29d3ff
jjackson112 <jjackson11211@gmail.com>
2025-11-13 22:23:29 -0500

    while loop + blog post card

--- home.php
+++ home.php
@@ -10,7 +10,48 @@
                 } else {
                     the_title();
                 }
-                }
             ?>
         </h1>
+        <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <?php if (have_post()) : ?>
+                <?php while (have_posts()) : the_post(); ?>
+
+                    <!-- Blog post card -->
+                    <article class="bg-white shadow rounded-lg overflow-hidden">
+                    <!-- Featured Image --> 
+                        <?php if (has_post_thumbnails()) : ?>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php the_post_thumbnail('medium_large', [
+                                    'class' => 'w-full h-48 object-cover'
+                                ]); ?>
+                            </a>
+                        <?php endif; ?>
+
+                        <div class="p-6">
+                            <h2 class="text-xl font-semibold mb-2">
+                                <a href="<?php the_permalink(); ?>" class="hover:text-sky-300">
+                                    <?php the_title(); ?>
+                                </a>
+                            </h2>
+
+                            <p class="text-md mb-2">
+                                <?php echo get_the_date(); ?>
+                            </p>
+
+                            <p class="excerpt text-gray-700 mb-4">
+                                <?php echo wp_trim_words(get_the_excerpt(), 28); ?>
+                            </p>
+
+                            <a href="<?php the_permalink(); ?>" class="text-sky-400 hover:text-sky-300 font-semibold">
+                                Read More →
+                            </a>
+                        </div>
+                    </article>
+
+                <?php endwhile; ?>
+            <?php else : ?>
+                <p>No posts found.</p>
+            <?php endif; ?>
+        </div>
+    </section>
 </main>
