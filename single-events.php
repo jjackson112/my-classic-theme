4f6522b1714cdcc61f75881866476ca86eaa927f
jjackson112 <jjackson11211@gmail.com>
2025-11-15 18:33:04 -0500

    custom post type styles

--- single-events.php
+++ single-events.php
@@ -5,46 +5,33 @@ Template for custom post type - events
 
 get_header(); ?>
 
-    <div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); 
-                $services = get_field('services');
-                $client = get_field('client');
-                $link = get_field('site_link');
-                $image_1 = get_field('image_1');
-                $image_2 = get_field('image_2');
-                $image_3 = get_field('image_3');
-                $size = "full"; 
-            ?>
-
-            <article class="case-study">
-                <aside class="case-study-sidebar">
-                    <h2><?php the_title(); ?></h2>
-                    <h4><?php echo $services; ?></h4>
-                    <h5><?php echo $client; ?></h5>
-                
-				    <?php the_content(); ?>
-
-                    <p class="read-more-link"><strong><a href="<?php echo $link; ?>">Visit Live Site</a></strong></p>
-                </aside>
-
-                <div class="case-study-images">
-                    <?php if($image_1) {
-                        echo wp_get_attachment_image( $image_1, $size );
-                    } ?>
-                    
-                    <?php if($image_2) {
-                        echo wp_get_attachment_image( $image_2, $size );
-                    } ?>
-                    
-                    <?php if($image_3) {
-                        echo wp_get_attachment_image( $image_3, $size );
+    <div class="max-w-4xl mx-auto p-6">
+		<?php while ( have_posts() ) : the_post(); 
+            $image = get_field('image');
+            $date = get_field('date');
+            $time = get_field('time');
+            $link = get_field('site_link');
+            $size = "full";
+        ?>
+        
+        <section>
+            <div class="mb-4 text-gray-600">
+                <h2 class="text-3xl font-bold text-sky-950 p-10"><?php the_title(); ?></h2>
+                <div class="flex justify-center">
+                    <?php if($image) {
+                        echo wp_get_attachment_image( $image, $size, false, array('class' => 'rounded shadow mb-4') );
                     } ?>
                 </div>
-            </article>
 
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+                <h4><?php echo $date; ?></h4>
+                <h5><?php echo $time; ?></h5>
+                
+				<?php the_content(); ?>
+                
+                <p class="hover:text-sky-300 mt-4"><a href="<?php echo ($link) ?>"Click here for more details</a></p>
+            </div>
+
+			<?php endwhile; ?>
+	        </div>
 
 <?php get_footer(); ?>
