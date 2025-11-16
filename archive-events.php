97f480e1a7e61e43f7d31c920f07f285a2efb786
jjackson112 <jjackson11211@gmail.com>
2025-11-16 16:23:09 -0500

    archive-events.php fixes

--- archive-events.php
+++ archive-events.php
@@ -7,29 +7,36 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <article class="bg-white rounded-xl shadow p-5 hover:shadow-lg transition">
+            <article class="bg-white rounded-lg shadow overflow-hidden">
                 
                 <?php if ( has_post_thumbnail() ) : ?>
-                    <?php the_post_thumbnail('medium', [
-                        'class' => 'w-full h-48 object-cover rounded-lg mb-4'
+                    <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('medium_large', [
+                        'class' => 'w-full h-48 object-cover'
                     ]); ?>
+                    </a>
                 <?php endif; ?>
 
-                <h2 class="text-xl font-semibold mb-2">
-                    <a href="<?php the_permalink(); ?>" class="hover:text-sky-500">
-                        <?php the_title(); ?>
+                <div class="p-6">
+                    <h2 class="text-xl font-semibold mb-2">
+                        <a href="<?php the_permalink(); ?>" class="hover:text-sky-300">
+                            <?php the_title(); ?>
+                        </a>
+                    </h2>
+
+                    <p class="text-md mb-2">
+                        <i class="fa-regular fa-clock text-sky-950"></i>
+                        <?php echo get_the_date(); ?>
+                    </p>
+
+                    <p class="excerpt text-gray-700 mb-4">
+                        <?php echo wp_trim_words(get_the_excerpt(), 20); ?>
+                    </p>
+
+                    <a href="<?php the_permalink(); ?>" class="text-sky-400 hover:text-sky-300 font-semibold">
+                        More information →
                     </a>
-                </h2>
-
-                <p class="text-gray-600 text-sm mb-4">
-                    <i class="fa-regular fa-clock text-sky-950"></i>
-                    <?php echo get_the_date(); ?>
-                </p>
-
-                <p class="text-gray-700">
-                    <?php echo wp_trim_words(get_the_excerpt(), 20); ?>
-                </p>
-
+                </div>
             </article>
 
         <?php endwhile; else : ?>
