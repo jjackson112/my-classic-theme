f7d86fc537cd7c6f9c7c77f2d21bff5ddf996cc4
jjackson112 <jjackson11211@gmail.com>
2025-11-15 21:06:03 -0500

    hover pagination

--- single.php
+++ single.php
@@ -26,9 +26,9 @@
             <?php endwhile; ?>
         </article>
 
-        <div class="max-w-3xl mx-auto px-6 mt-12 flex justify-between">
-            <div><?php previous_post_link('%link', '← Previous'); ?></div>
-            <div><?php next_post_link('%link', 'Next →'); ?></div>
+        <div class="max-w-3xl mx-auto px-6 mt-12 pb-10 flex justify-between font-semibold">
+            <div class="hover:text-sky-300"><?php previous_post_link('%link', '← Previous'); ?></div>
+            <div class="hover:text-sky-300"><?php next_post_link('%link', 'Next →'); ?></div>
         </div>
     </section>
 </main>
