4bbb2ae51725f6acf556e61ed6322f31958ac38a
jjackson112 <jjackson11211@gmail.com>
2025-11-17 14:26:27 -0500

    hover

--- home.php
+++ home.php
@@ -37,7 +37,7 @@ Template Name: Blog */
                                 $categories = get_the_category();
                                 if ($categories) : $cat = $categories[0]; ?>
                                     <a href="<?php echo esc_url(get_category_link($cat->term_id)); ?>"
-                                        class="border border-sky-500 text-sky-500 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded-full">
+                                        class="border border-sky-500 text-sky-500 text-xs font-semibold hover:bg-sky-100 mr-2 px-2.5 py-0.5 rounded-full">
                                         <?php echo esc_html($cat -> name); ?>
                                     </a>
                                 <?php endif; ?>
