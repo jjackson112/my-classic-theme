698886e3bbc23cecf70caed00c0ce8abdd28d4b3
jjackson112 <jjackson11211@gmail.com>
2025-11-17 19:14:55 -0500

    hero text edited

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
     style="background-image: url('<?php echo get_template_directory_uri(); ?>/img/smile.png?v=<?php echo time(); ?>');"
     class="bg-cover bg-no-repeat bg-center min-h-screen flex flex-col">
     <div class="flex-grow flex items-start justify-center pt-32">
-        <h1 class="text-2xl text-neutral-50 text-center">
+        <h1 class="sm:text-2xl lg:text-3xl text-neutral-50 text-center">
             "I capture the realism behind feminism, no filter."
         </h1>
     </div> 
